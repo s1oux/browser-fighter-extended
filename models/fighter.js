220fdaaec05ed8f1940e305e52a67cc15670283a
andanfed <andanfe@gmail.com>
2020-05-13 17:19:55 +0300

    Change models

--- models/fighter.js
+++ models/fighter.js
@@ -1,6 +1,18 @@
-exports.fighter = {
-    "id": "",
-    "name": "",
-    "health": 100,
-    "power": 0
+class Fighter {
+  constructor({ name, health, power, defense }) {
+    this.name = name;
+    this.health = Number(health);
+    this.power = Number(power);
+    this.defense = Number(defense);
+  }
 }
+
+// exports.fighter = {
+//   id: Number, // fighterID
+//   name: String, // fighterName
+//   health: Number, // fighterHealth
+//   power: Number, // fighterPower
+//   defense: Number, // add defense ??
+// };
+
+module.exports.Fighter = Fighter;
--- models/user.js
+++ models/user.js
@@ -1,8 +1,19 @@
-exports.user = {
-    id: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: '',
-    password: '' // min 3 symbols
+class User {
+  constructor({ firstName, lastName, email, phoneNumber, password }) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.password = password;
+  }
 }
+// exports.User = {
+//   id: Number, // userID
+//   firstName: String, // userFirstName
+//   lastName: String, // userLastName
+//   email: String, // userEmail
+//   phoneNumber: String, // userNumber
+//   password: String, // userPassword
+// };
+
+module.exports.User = User;
