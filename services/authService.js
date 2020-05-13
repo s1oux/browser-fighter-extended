79ab964b3483b65793c54830ef030b1071f381aa
andanfed <andanfe@gmail.com>
2020-05-13 17:22:33 +0300

    Add business logic for routes in services

--- services/authService.js
+++ services/authService.js
@@ -1,13 +1,14 @@
 const UserService = require('./userService');
 
 class AuthService {
-    login(userData) {
-        const user = UserService.search(userData);
-        if(!user) {
-            throw Error('User not found');
-        }
-        return user;
+  login(userData) {
+    const user = UserService.search(userData);
+    if (!user) {
+      // throw Error('User not found');
+      return null;
     }
+    return user;
+  }
 }
 
 module.exports = new AuthService();
