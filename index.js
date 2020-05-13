9d579dd9a5aedf672c3c3c992f5eca3706393b81
andanfed <andanfe@gmail.com>
2020-05-13 17:20:28 +0300

    Minor changes after cloning

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 require('./config/db');
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +13,9 @@ routes(app);
 
 app.use('/', express.static('./client/build'));
 
-const port = 3050;
-app.listen(port, () => {});
+const port = process.env.PORT || 7721;
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
 
 exports.app = app;
